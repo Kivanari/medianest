2a1768a769f03d8958c9d446671f873d891a4483
Kirill <kiriklrukit@yandex.ru>
2025-05-26 04:02:11 +0300

    1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,13 @@
-
 import React from 'react';
-import { Button, Link } from '@mui/material';  // Импортируем Link и Button
+import { Button } from '@mui/material';  // Импортируем Button из Material-UI
+import { Link } from 'react-router-dom';  // Импортируем Link из react-router-dom для навигации
 
 const HomePage = () => {
   return (
     <div>
       <h1>Добро пожаловать в MediaNest!</h1>
       
+      {/* Используем Link из react-router-dom для перехода */}
       <p>Перейдите на <Link to="/register">страницу регистрации</Link>.</p>
       <Button variant="contained" color="primary">
         Пример кнопки
